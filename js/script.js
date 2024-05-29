c3cbfffd7adb062d8e5d0ddd6c564123cbea46ab
Arsya Amalia Ristias <arsyaamalia1@gmail.com>
2024-05-29 23:37:30 +0700

    finish section 5 & countdown

--- js/script.js
+++ js/script.js
@@ -17,3 +17,43 @@ function slideImage(){
 }
 
 window.addEventListener('resize', slideImage);
+
+let $days = document.getElementById('hari');
+let $hours = document.getElementById('jam');
+let $minutes = document.getElementById('menit');
+let $seconds = document.getElementById('detik');
+
+// Calculate the date 51 days from now
+let now = new Date();
+let daysToAdd = 51;
+let countdownDate = new Date(now.getTime() + daysToAdd * 24 * 60 * 60 * 1000);
+
+setInterval(function() {
+    var now = new Date();
+    var timeleft = (countdownDate - now) / 1000;
+    updateclock(timeleft);
+}, 1000);
+
+function updateclock(remainingTime) {
+
+    let days = Math.floor(remainingTime / 86400);
+    remainingTime -= days * 86400;
+
+    let hours = Math.floor(remainingTime / 3600) % 24;
+    remainingTime -= hours * 3600;
+
+    let minutes = Math.floor(remainingTime / 60) % 60;
+    remainingTime -= minutes * 60;
+
+    let seconds = Math.floor(remainingTime % 60);
+
+    $days.innerHTML = Number(days);
+    $hours.innerHTML = Number(hours);
+    $minutes.innerHTML = Number(minutes);
+    $seconds.innerHTML = Number(seconds);
+
+}
+
+function Number(number) {
+    return number < 10 ? "0" + number : number;
+}
